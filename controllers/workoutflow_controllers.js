ba40480bbe5b0195c946b98423904e0d008c76b1
Turan Ketene <turank97@gmail.com>
2019-11-06 20:12:24 -0800

    did some routing

--- controllers/workoutflow_controllers.js
+++ controllers/workoutflow_controllers.js
@@ -12,7 +12,27 @@ router.get("/", function(req, res) {
       mscl: data
     };
     console.log(hbsObject);
-    res.render("index", hbsObject);
+    res.render("home", hbsObject);
+  });
+});
+
+router.get("/muscle_building", function(req, res) {
+  workoutflow.all(function(data) {
+    var hbsObject = {
+      mscl: data
+    };
+    console.log(hbsObject);
+    res.render("muscle_building", hbsObject);
+  });
+});
+
+router.get("/", function(req, res) {
+  workoutflow.all(function(data) {
+    var hbsObject = {
+      mscl: data
+    };
+    console.log(hbsObject);
+    res.render("cardio", hbsObject);
   });
 });
 
@@ -27,22 +47,6 @@ router.get("/", function(req, res) {
 //   });
 // });
 
-// router.put("/api/cats/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
-
-//   console.log("condition", condition);
-
-//   cat.update({
-//     sleepy: req.body.sleepy
-//   }, condition, function(result) {
-//     if (result.changedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
 
 // router.delete("/api/cats/:id", function(req, res) {
 //   var condition = "id = " + req.params.id;
