a415bf05f49f3e58d11ed2f72b3f0e6ded328b86
Turan Ketene <turank97@gmail.com>
2019-11-13 21:27:17 -0800

    did some stuff

--- controllers/workoutflow_controllers.js
+++ controllers/workoutflow_controllers.js
@@ -37,7 +37,6 @@ router.post("/muscle_buildingCreate", function(req, res) {
   });
 });
 
-
 router.put("/muscle_buildingUpdate", function(req, res) {
   workouts.muscle_building.update(req.body.mb_id, function(result){
     console.log(result);
@@ -65,18 +64,6 @@ router.post("/cardioCreate", function(req, res) {
     res.redirect("/cardio");
   });
 });
-// router.delete("/api/cats/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
-
-//   cat.delete(condition, function(result) {
-//     if (result.affectedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
 
 // Export routes for server.js to use.
 module.exports = router;
--- models/workoutflow.js
+++ models/workoutflow.js
@@ -17,22 +17,6 @@ var muscle_building = {
       cb(res);
     });
   },
-  // // The variables cols and vals are arrays.
-  // create: function(cols, vals, cb) {
-  //   orm.create("muscle_building", cols, vals, function(res) {
-  //     cb(res);
-  //   });
-  // },
-  // update: function(objColVals, condition, cb) {
-  //   orm.update("muscle_building", objColVals, condition, function(res) {
-  //     cb(res);
-  //   });
-  // },
-  // delete: function(condition, cb) {
-  //   orm.delete("muscle_building", condition, function(res) {
-  //     cb(res);
-  //   });
-  // }
 };
 
 var cardio = {
